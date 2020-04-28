cb8479181a7b7a7ba117c59d514450d8b16b6163
thoughtless449 <64351086+thoughtless449@users.noreply.github.com>
2020-04-28 17:11:58 +0000

    Add files via upload

---
+++ Links.js
@@ -0,0 +1,20 @@
+links=
+	{ topmenu : [
+				  { title: "GMail", href:"http://www.google.com/mail"}
+		],
+	items : [
+	{ column: 1,
+	  title: "Top",
+	  links: [
+		{title:"MuseScore", href:"https://musescore.com"}
+	  ]
+	},
+	{ column: 1,
+	  title: "Foto's",
+	  links: [
+				{title: "Remove BG", href:"https://remove.bg"}
+	  ]
+	},
+
+	  ]
+	  };
